4f5a6e866f711b17d60a9777dcfb5a28ffab75bb
𝙙𝙞𝙭𝙚 <135438002+dixe1@users.noreply.github.com>
2025-12-14 17:10:47 +0100

    Enhance write function to hide/show cursor
    Added cursor hide and show functionality during text output.

--- console_writer/console_writer.cpp
+++ console_writer/console_writer.cpp
@@ -11,27 +11,34 @@
     delayS -> Additional delay after spaces
 */
 
-namespace consoleTools {
-    void write(const std::string& text, int color, int delayCh, int delayS) {
-        if (color != 0) {
+namespace consoleTools
+{
+    void write(const std::string& text, int color, int delayCh, int delayS)
+    {
+		std::cout << "\033[?25l"; // Hide cursor
+
+        if (color != 0)
+        {
             std::cout << "\033[" << color << "m";
-            for (size_t i = 0; i < text.length(); i++) {
+            for (size_t i = 0; i < text.length(); i++)
+            {
                 std::cout << text[i];
                 std::this_thread::sleep_for(std::chrono::milliseconds(delayCh));
-                if (text[i] == ' ') {
+                if (text[i] == ' ')
                     std::this_thread::sleep_for(std::chrono::milliseconds(delayS));
-                }
             }
             std::cout << "\033[0m";
         }
-        else {
-            for (size_t i = 0; i < text.length(); i++) {
+        else
+        {
+            for (size_t i = 0; i < text.length(); i++)
+            {
                 std::cout << text[i];
                 std::this_thread::sleep_for(std::chrono::milliseconds(delayCh));
-                if (text[i] == ' ') {
+                if (text[i] == ' ')
                     std::this_thread::sleep_for(std::chrono::milliseconds(delayS));
-                }
             }
         }
+		std::cout << "\033[?25h"; // Show cursor
     }
-}
+} // namespace consoleTools
