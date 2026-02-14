ea42daaebd08ddd3a6a2294c23e48a53e04eb04e
Bartłomiej Dobrowolski <annim1.5.1@gmail.com>
2026-02-14 16:24:19 +0100

    allow spaces in config.txt

--- ConfigLoader/ConfigLoader.h
+++ ConfigLoader/ConfigLoader.h
@@ -11,17 +11,15 @@
 
 class ConfigLoader
 {
-    public:
-    static std::unordered_map<std::string, std::string> load(const std::string& pathAndFileName)
+public:
+
+    static std::unordered_map<std::string, std::string>
+    load(const std::string& pathAndFileName)
     {
         std::unordered_map<std::string,std::string> config;
 
         std::fstream file(pathAndFileName, std::ios::in);
-        if (!file.is_open())
-            std::cerr << "Error opening file" << std::endl;
-
-        if (!file.good())
-            std::cerr << "Error file not good" << std::endl;
+        checkFile(file);
 
         std::string line;
         while (std::getline(file, line))
@@ -34,6 +32,9 @@ class ConfigLoader
             std::string key  = line.substr(0, pos);
             std::string value = line.substr(pos + 1);
 
+            deleteSpaces(value);
+            deleteSpaces(key);
+
             config[key] = value;
         }
 
@@ -41,4 +42,27 @@ class ConfigLoader
         return config;
     }
 
+
+private:
+
+    // Helper functions
+    static void checkFile(const std::fstream& file)
+    {
+        if (!file.is_open())
+            throw std::runtime_error("Error opening file");
+
+        if (!file.good())
+            throw std::runtime_error("Error file not good");
+    }
+    static void deleteSpaces(std::string& line)
+    {
+        if (line.empty())
+            return;
+
+        if (line.at(line.length()-1) == ' ')
+            line.erase(line.length()-1, line.length());
+
+        if (line.at(0) == ' ')
+            line.erase(0, 1);
+    }
 };
